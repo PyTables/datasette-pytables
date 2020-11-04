54aeb4a8918f31b273875646d7a56a1c4da34564
Javier Sancho <jsf@jsancho.org>
2020-11-04 18:06:51 +0100

    Fix html tests

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,28 +1,28 @@
-from .fixtures import app_client
+from .fixtures import app_client, app_client_with_hash
 
 def test_homepage(app_client):
-    response = app_client.get('/', gather_request=False)
+    response = app_client.get('/')
     assert response.status == 200
     assert 'test_tables' in response.text
 
-def test_database_page(app_client):
-    response = app_client.get('/test_tables', allow_redirects=False, gather_request=False)
+def test_database_page(app_client_with_hash):
+    response = app_client_with_hash.get('/test_tables', allow_redirects=False)
     assert response.status == 302
-    response = app_client.get('/test_tables', gather_request=False)
+    response = app_client_with_hash.get('/test_tables')
     assert 'test_tables' in response.text
 
 def test_table(app_client):
-    response = app_client.get('/test_tables/%2Fgroup1%2Ftable1', gather_request=False)
+    response = app_client.get('/test_tables/%2Fgroup1%2Ftable1')
     assert response.status == 200
 
 def test_table_row(app_client):
-    response = app_client.get('/test_tables/%2Fgroup1%2Ftable1/50', gather_request=False)
+    response = app_client.get('/test_tables/%2Fgroup1%2Ftable1/50')
     assert response.status == 200
 
 def test_array(app_client):
-    response = app_client.get('/test_tables/%2Fgroup1%2Farray2', gather_request=False)
+    response = app_client.get('/test_tables/%2Fgroup1%2Farray2')
     assert response.status == 200
 
 def test_array_row(app_client):
-    response = app_client.get('/test_tables/%2Fgroup1%2Farray2/1050', gather_request=False)
+    response = app_client.get('/test_tables/%2Fgroup1%2Farray2/1050')
     assert response.status == 200
